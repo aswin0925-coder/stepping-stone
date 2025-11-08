fa74e3895bc987924911d0713ec2506291849aff
aswin0925-coder <aswin0925@ku.edud.np>
2025-11-08 17:03:54 +0545

    finally the wholesome has been changed

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-/*java script is added now*/
+/*java script is linked and i tried to do it but cant be done*/
+document.getElementById("a1").addEventListener("click", function() {
+    alert("Button was clicked!");
+});
